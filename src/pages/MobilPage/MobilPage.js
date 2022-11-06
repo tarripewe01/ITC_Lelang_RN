6d14406e334509821b0353e57e565f7bf7830b50
tarripewe <tarripewe01@gmail.com>
2022-11-06 13:32:55 +0700

    Integrasi API All Produk

--- src/pages/MobilPage/MobilPage.js
+++ src/pages/MobilPage/MobilPage.js
@@ -36,22 +36,8 @@ const MobilPage = ({navigation}) => {
   }, []);
 
   return (
-    <View
-      style={{
-        paddingHorizontal: 15,
-        marginTop: 20,
-        flex: 1,
-      }}>
-      <View
-        style={{
-          borderWidth: 1,
-          borderColor: Colors.blue,
-          height: 40,
-          padding: 10,
-          borderRadius: 10,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}>
+    <View style={styles.container}>
+      <View style={styles.contaiSearch}>
         <TextInput placeholder="Cari Mobil ..." />
         <MaterialCommunityIcons name="magnify" color={Colors.blue} size={24} />
       </View>
@@ -64,36 +50,21 @@ const MobilPage = ({navigation}) => {
             onPress={() => {
               navigation.navigate('Detail Kendaraan', {item: item});
             }}>
-            <Card
-              style={{
-                width: wp('46%'),
-                marginRight: 10,
-                marginTop: 10,
-                marginBottom: 10,
-                height: 300,
-              }}>
+            <Card style={styles.card}>
               <Card.Cover
                 source={{
                   uri: 'https://itc-finance.herokuapp.com' + item.photo_path[0],
                 }}
               />
               <Card.Content style={{paddingHorizontal: 5}}>
-                <Text style={{fontWeight: 'bold', fontSize: 13}}>
-                  {item.nama_produk}
-                </Text>
+                <Text style={styles.text}>{item.nama_produk}</Text>
                 <View style={{flexDirection: 'row', marginTop: 3}}>
                   <MaterialCommunityIcons
                     name="calendar"
                     color={Colors.blackJet}
                     size={12}
                   />
-                  <Text
-                    style={{
-                      fontWeight: 'bold',
-                      fontSize: 10,
-                      color: Colors.blackJet,
-                      marginLeft: 3,
-                    }}>
+                  <Text style={styles.text}>
                     {moment(item?.tanggal_mulai).format('DD/MM/YYYY')}
                   </Text>
                 </View>
@@ -103,23 +74,9 @@ const MobilPage = ({navigation}) => {
                     color={Colors.blackJet}
                     size={12}
                   />
-                  <Text
-                    style={{
-                      fontWeight: 'bold',
-                      fontSize: 10,
-                      color: Colors.blackJet,
-                      marginLeft: 3,
-                    }}>
-                    ITC {item.cabang}
-                  </Text>
+                  <Text style={styles.text}>ITC {item.cabang}</Text>
                 </View>
-                <Text
-                  style={{
-                    fontWeight: 'bold',
-                    fontSize: 16,
-                    color: Colors.blue,
-                    marginTop: 10,
-                  }}>
+                <Text style={styles.harga}>
                   {currencyFormatter.format(item?.harga, {
                     code: 'IDR',
                   })}
@@ -135,4 +92,39 @@ const MobilPage = ({navigation}) => {
 
 export default MobilPage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 15,
+    marginTop: 20,
+    flex: 1,
+  },
+  contaiSearch: {
+    borderWidth: 1,
+    borderColor: Colors.blue,
+    height: 40,
+    padding: 10,
+    borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  card: {
+    width: wp('46%'),
+    marginRight: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    height: 300,
+  },
+  title: {fontWeight: 'bold', fontSize: 13},
+  text: {
+    fontWeight: 'bold',
+    fontSize: 10,
+    color: Colors.blackJet,
+    marginLeft: 3,
+  },
+  harga: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: Colors.blue,
+    marginTop: 10,
+  },
+});
